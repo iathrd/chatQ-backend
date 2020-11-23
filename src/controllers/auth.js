c68cb5b298f222a9b2dd0faa05745d19ade9d8e3
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-23 14:54:19 +0800

    added validation phone number and added jwt on create Phone

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,36 +1,43 @@
 const { User } = require("../models");
 const { response } = require("../helpers/response");
 const { UserSchema } = require("../helpers/validation_schema");
-const {signAccesToken} = require('../helpers/jwt_init')
+const { signAccesToken } = require("../helpers/jwt_init");
 
 module.exports = {
   createPhone: async (req, res) => {
     try {
+      const { phoneNumber } = req.body;
+      const find = await User.findOne({ where: { phoneNumber } });
+      if (find) {
+        response(res, "Phone number already registered!", {}, false, 400);
+      }
       const result = await User.create({ ...req.body });
-      result
-        ? response(res, "Phone created")
-        : response(res, "Failed to create", {}, false, 400);
+      if (result) {
+        const token = await signAccesToken(result.id);
+        response(res, "Phone Created", { token });
+      } else {
+        response(res, "Failed to create", {}, false, 400);
+      }
     } catch (error) {}
   },
-  createUser: (req, res) => {
+  createUser: async (req, res) => {
     try {
-        const {aud} = req.payload
-        let data = await UserSchema.validateAsync({...req.body})
-      if (req.files !== undefined) {
+      const { aud } = req.payload;
+      let data = await UserSchema.validateAsync({ ...req.body });
+      if (req.file !== undefined) {
         let { path } = req.file;
         path = path.replace(/\\/g, "/");
         data = {
-            ...data,
-            picture: path
-        }
+          ...data,
+          avatar: path,
+        };
       }
-      const result = await User.update(data,{where:{id:aud}})
-      result 
-      ? response(res,'User Created !',{data})
-      : response(res,'Failed to create!',{},false,400)
-
+      const result = await User.update(data, { where: { id: aud } });
+      result
+        ? response(res, "User Created !", { data })
+        : response(res, "Failed to create!", {}, false, 400);
     } catch (error) {
-        error.isJoi === true && response(res, error.message, false, 400)
+      error.isJoi === true && response(res, error.message, false, 400);
     }
   },
 };
