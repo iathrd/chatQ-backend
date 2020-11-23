811222197f3ce703fe27a65200aacebae3473497
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-23 14:35:30 +0800

    created route for crete user

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,9 @@
 const auth = require("../controllers/auth");
 const route = require("express").Router();
 const { upload } = require("../helpers/init_multer");
+const {verifyAccessToken} = require('../helpers/jwt_init')
 
 route.post("/createPhone", auth.createPhone);
+route.post('/creteUser',verifyAccessToken,upload.single('picture'), auth.createUser)
 
 module.exports = route;
