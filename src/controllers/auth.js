c73ddcd885b553bbb2b8c949a78c0b26e1216dd2
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-23 14:12:27 +0800

    created create Phone number for user

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,15 @@
+const{User} = require('../models')
+const {response} = require('../helpers/response')
+
 module.exports = {
-    createUser:(req,res)=> {
-        
+    createPhone: async(req,res)=> {
+        try {
+            const result = await User.create({...req.body})
+            result 
+            ? response(res,'Phone created')
+            : response(res,'Failed to create',{},false,400)
+        } catch (error) {
+            
+        }
     }
 }
