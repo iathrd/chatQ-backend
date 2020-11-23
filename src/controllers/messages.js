c25bdb301c82470a046644234812650029365b05
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-23 16:18:43 +0800

    created create message controllers

--- src/controllers/messages.js
+++ src/controllers/messages.js
@@ -1,5 +1,15 @@
+const { Message } = require("../models");
+const { response } = require("../helpers/response");
+
 module.exports = {
-    creteMessage:(req,res)=>{
-        
-    }
-}
+  creteMessage: async (req, res) => {
+    try {
+        // const { aud } = req.payload
+    //   const { content, sender, receiver } = req.body;
+      const result = await Message.create({...req.body})
+      result
+      ? response(res,'Message sent',{data:{...req.body}})
+      : response(res,"Can't send message try again!",{},false,400)
+    } catch (error) {}
+  },
+};
