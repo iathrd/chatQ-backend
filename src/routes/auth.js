c4ec66089c535dd037be332e17447abf25e1b31f
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-23 14:13:02 +0800

    created route post for crate phone number

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,6 @@ const auth = require("../controllers/auth");
 const route = require("express").Router();
 const { upload } = require("../helpers/init_multer");
 
-route.post("/", upload.single("picture"), auth.createUser);
+route.post("/createPhone", auth.createPhone);
 
 module.exports = route;
