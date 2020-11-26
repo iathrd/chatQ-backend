c725e6f93612c354d12ba35898dd31b849ba2a80
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-11-26 12:29:25 +0800

    fixed craete phone number controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -10,14 +10,15 @@ module.exports = {
       const find = await User.findOne({ where: { phoneNumber } });
       if (find) {
         const token = await signAccesToken(find.id);
-        response(res, "Phone number already registered!", {token})
-      }
-      const result = await User.create({ ...req.body });
-      if (result) {
-        const token = await signAccesToken(result.id);
-        response(res, "Phone Created", { token });
+        response(res, "Phone number already registered!", { token });
       } else {
-        response(res, "Failed to create", {}, false, 400);
+        const result = await User.create({ ...req.body });
+        if (result) {
+          const token = await signAccesToken(result.id);
+          response(res, "Phone Created", { token });
+        } else {
+          response(res, "Failed to create", {}, false, 400);
+        }
       }
     } catch (error) {}
   },
@@ -41,39 +42,37 @@ module.exports = {
       error.isJoi === true && response(res, error.message, false, 400);
     }
   },
-  updateUser: async (req,res)=> {
-      try {
-        const { aud } = req.payload;
-        let data = await UserSchema.validateAsync({ ...req.body });
-        if (req.file !== undefined) {
-          let { path } = req.file;
-          path = path.replace(/\\/g, "/");
-          data = {
-            ...data,
-            avatar: path,
-          };
-        }
-        const result = await User.update(data, { where: { id: aud } });
-        result
-          ? response(res, "Sucssesfullt Updated!", { data })
-          : response(res, "Failed to Updated!", {}, false, 400);
-      } catch (error) {
-        error.isJoi === true && response(res, error.message, false, 400);  
+  updateUser: async (req, res) => {
+    try {
+      const { aud } = req.payload;
+      let data = await UserSchema.validateAsync({ ...req.body });
+      if (req.file !== undefined) {
+        let { path } = req.file;
+        path = path.replace(/\\/g, "/");
+        data = {
+          ...data,
+          avatar: path,
+        };
       }
+      const result = await User.update(data, { where: { id: aud } });
+      result
+        ? response(res, "Sucssesfullt Updated!", { data })
+        : response(res, "Failed to Updated!", {}, false, 400);
+    } catch (error) {
+      error.isJoi === true && response(res, error.message, false, 400);
+    }
   },
-  deleteUser: async (req,res)=>{
-      try {
-          const {aud} = req.payload;
-          const result = await User.destroy({
-              where:{
-                  id:aud
-              }
-          })
-          result
-          ? response(res,'Succesfuly deleted')
-          : response(res,'Failed to deleted',{},false,400)
-      } catch (error) {
-          
-      }
-  }
+  deleteUser: async (req, res) => {
+    try {
+      const { aud } = req.payload;
+      const result = await User.destroy({
+        where: {
+          id: aud,
+        },
+      });
+      result
+        ? response(res, "Succesfuly deleted")
+        : response(res, "Failed to deleted", {}, false, 400);
+    } catch (error) {}
+  },
 };
